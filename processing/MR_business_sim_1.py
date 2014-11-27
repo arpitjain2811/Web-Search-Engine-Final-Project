c99074b2b59ab2d9c0279a362ffa0cd99774bf1b
MarkAWard <markmaw@gmail.com>
2014-11-27 00:51:40 -0500

    custom stopword list

--- processing/MR_business_sim_1.py
+++ processing/MR_business_sim_1.py
@@ -1,14 +1,14 @@
 from mrjob.job import MRJob
 from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
 import json
 import re
 import string
 
+stopwords = ["els","tell","expect","thought","noth","usual","anyth","let","wasn","enough","put","told","d","actual","though","took","found","everyth","ll","us","mani","area","someth","feel","came","anoth","said","re","seem","ever","still","everi","made","sinc","around","sure","two","think","lot","see","ask","find","right","locat","didn","give","peopl","way","use","first","went","say","could","thing","much","take","got","never","day","m","tri","nice","come","ve","alway","know","want","well","work","need","also","make","look","even","realli","servic","back","would","good","place","great","one","like","time","go","get","i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"]
 
 PUNC = re.compile('[%s]' % re.escape(string.punctuation))
 WORD = re.compile(r"[\w']+")
-STOP = re.compile(r'\d+|[^\x00-\x7F]+|\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
+STOP = re.compile(r'\d+|[^\x00-\x7F]+|\b(' + r'|'.join(stopwords) + r')\b\s*')
 PS = PorterStemmer()
 
 class MRBusinessSim1(MRJob):
